08fc44cc82ee6610ba975469f759d412f284743f
Agata Musiuk <agata.musiuk@gmail.com>
2019-09-22 11:26:37 +0200

    Add google oauth

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '510503452100-evq9ace5h4936ghn29ihss8gvj8uluht.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'oD5v7kPYiGJFvPTt4a94KmhP',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
